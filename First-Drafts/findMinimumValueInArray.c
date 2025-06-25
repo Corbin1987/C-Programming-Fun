002762683173df5343526e6a555633dc5419afb5
Corbin Via <corbinvia@gmail.com>
2025-06-25 16:51:14 -0500

    Changed function to accept a second parameter for array length

--- First-Drafts/findMinimumValueInArray.c
+++ First-Drafts/findMinimumValueInArray.c
@@ -10,13 +10,7 @@
 
 //output minimum value
 
-int findMinimumValueInArray(int arrayToCheck[]) {
-	int arrayLength;
-	
-	if (arrayToCheck[0]) {
-		arrayLength = sizeof(arrayToCheck) / sizeof(arrayToCheck[0]);
-	}
-
+int findMinimumValueInArray(int arrayToCheck[], int arrayLength) {
 	if (arrayLength == 0) {
 		printf("The input array does not contain any data.\n");
 		return 0;
@@ -25,16 +19,19 @@ int findMinimumValueInArray(int arrayToCheck[]) {
 	int minimumValue = arrayToCheck[0];
 
 	for (int i = 0; i < arrayLength; i ++) {
+		printf("Loop iteration: %d", i);
 		if (arrayToCheck[i] < minimumValue) {
 			minimumValue = arrayToCheck[i];
+			printf("If statement met\n");
 		}
 	}
 	return minimumValue;
 }
 
 int main() {
-	int testarray[5] = {2, 3, 4, 5, 1};
-	int result = findMinimumValueInArray(testarray);
+	int testarray[] = {2, 3, 4, 5, 1};
+	int arrayLength = sizeof(testarray) / sizeof(testarray[0]);
+	int result = findMinimumValueInArray(testarray, arrayLength);
 	printf("%d\n", result);
 	return 0;
 }
