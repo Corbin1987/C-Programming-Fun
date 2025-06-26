1a509e14f46a590726edb097bd629d03951186ea
Corbin1987 <corbinvia@gmail.com>
2025-06-25 22:14:59 -0500

    Refactored calculateFactorial function and tested; function returned the correct result

--- First-Drafts/calculateFactorial.c
+++ First-Drafts/calculateFactorial.c
@@ -4,16 +4,17 @@
 //e.g., 5! = (5 x 4 x 3 x 2 x 1)
 
 int calculateFactorial (int numberToFactorial) {
+	//printf("%i\n", numberToFactorial);
 	int factorialResult = 1;
-	if (numberToFactorial == 0) {
-		return factorialResult;
-	}
 	if (numberToFactorial > 0) {
-		return factorialResult * calculateFactorial(numberToFactorial - 1);
+		return numberToFactorial * calculateFactorial(numberToFactorial - 1);
+	} else {
+		return 1;
 	}
 }
 
 int main () {
 	int result = calculateFactorial(5);
 	printf("%d\n", result);
+	return 0;
 }
