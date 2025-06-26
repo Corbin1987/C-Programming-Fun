eccf456622978293c4936a99e00d80f43f84b3fa
Corbin1987 <corbinvia@gmail.com>
2025-06-25 21:46:19 -0500

    Initial attempt at recursive function; will need to refactor, as this is not returning the correct value

--- First-Drafts/calculateFactorial.c
+++ First-Drafts/calculateFactorial.c
@@ -3,6 +3,17 @@
 // Given a whole integer, calculate and return the factorial value
 //e.g., 5! = (5 x 4 x 3 x 2 x 1)
 
-int calculateFactorial (int numberToMultiply) {
-	
+int calculateFactorial (int numberToFactorial) {
+	int factorialResult = 1;
+	if (numberToFactorial == 0) {
+		return factorialResult;
+	}
+	if (numberToFactorial > 0) {
+		return factorialResult * calculateFactorial(numberToFactorial - 1);
+	}
+}
+
+int main () {
+	int result = calculateFactorial(5);
+	printf("%d\n", result);
 }
